eb4db0c9762894f42f42810e9b7875ee802fd7ac
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-03-14 14:26:23 -0700

    numbers practice 1

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,17 +1,29 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float aFloat = -1f;
+    System.out.println("The float value is: " + aFloat);
 
     // Create an int with a positive value and assign it to a variable
+    int anInt = 14;
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    int remainderByThree = anInt % 3;
+    System.out.println("Remainder being divided by 3: " + remainderByThree);
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+    if (anInt % 2 == 0) {
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
+    }
 
     // Divide the number by another number using integer division
+    int divisionResult = anInt / 4;
+    System.out.println("Result of " + anInt + " / 4 (integer division): " + divisionResult);
 
     /*
      * Reminder!
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,4 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
-- 
+- Had to test different ints in numberpractice to verify the modulus working
