2ddb641dcc2421913d4f8fe413ce3f7bddf2f4e0
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-03-14 14:45:29 -0700

    arraypractice & string practice 3, 4

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,17 +1,34 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
+    String[] planets = new String[4];
 
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
+    planets[0] = "Mercury";
+    planets[1] = "Venus";
+    planets[2] = "Earth";
+    planets[3] = "Mars";
 
     // Get the value of the array at index 2
+    String thirdPlanet = planets[2];
+    System.out.println("The planet at index 2 is: " + thirdPlanet);
 
     // Get the length of the array
+    int arrayLength = planets.length;
+    System.out.println("The array has " + arrayLength + " slots.");
 
     // Iterate over the array using a traditional for loop and print out each item
+    System.out.println("--- Traditional Loop ---");
+    for (int i = 0; i < planets.length; i++) {
+        System.out.println("Index " + i + ": " + planets[i]);
+    }
 
     // Iterate over the array using a for-each loop and print out each item
+    System.out.println("--- For-Each Loop ---");
+    for (String planet : planets) {
+        System.out.println(planet);
+    }
 
     /*
      * Reminder!
--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,25 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
+    String myStr = "Hello World";
 
     // Find the length of the string
+    int length = myStr.length();
+    System.out.println("Length: " + length);
 
     // Concatenate (add) two strings together and reassign the result
+    myStr = myStr + "!";
 
     // Find the value of the character at index 3
+    char charAtThree = myStr.charAt(3);
+    System.out.println("Character at index 3: " + charAtThree);
 
     // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
+    boolean containsAbc = myStr.contains("abc");
+    System.out.println("Contains 'abc'?: " + containsAbc);
 
     // Iterate over the characters of the string, printing each one on a separate line
+    for (int i = 0; i < myStr.length(); i++) {
+        System.out.println(myStr.charAt(i));
+    }
 
     // Create an ArrayList of Strings and assign it to a variable
+    List<String> fruitList = new ArrayList<>();
 
     // Add multiple strings to the List (OK to do one-by-one)
+    fruitList.add("Apple");
+    fruitList.add("Guava");
+    fruitList.add("Cherry");
 
     // Join all of the strings in the list together into a single string separated by commas
     // Use a built-in method to achieve this instead of using a loop
+    String joinedFruits = String.join(", ", fruitList);
 
     // Check whether two strings are equal
+    boolean areEqual = "Apple".equals(fruitList.get(0));
+    System.out.println("Is index 0 'Apple'?: " + areEqual);
 
     /*
      * Reminder!
@@ -28,5 +49,6 @@ public class StringPractice {
      * 
      * We use == when comparing primitives (e.g. int or char).
      */
+    System.out.println("Joined String: " + joinedFruits);
   }
 }
